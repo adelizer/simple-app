6d18e8a6007bda549189256146536d7a1f8e89ee
Mo <m.adel.abdelhady@gmail.com>
2023-08-23 21:50:05 +0200

    testing users get and post routes

--- app/main.py
+++ app/main.py
@@ -1,4 +1,11 @@
+from pydantic import BaseModel
 from fastapi import FastAPI
+
+
+class UserData(BaseModel):
+    name: str
+    age: int
+
 
 app = FastAPI()
 
@@ -6,3 +13,13 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello, FastAPI!"}
+
+
+@app.get("/users/{user_id}")
+def get_user(user_id: int):
+    return {"user": user_id}
+
+
+@app.post("/details")
+def get_user_details(user: UserData):
+    return user
